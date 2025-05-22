311455703e74b9afcf39ba56ef9208a77f0f9c88
Kornchanok Iednusorn <kornchanokiednusorn@gmail.com>
2025-05-22 19:19:53 +0700

    refactor: Restructure frontend project with Nx

--- frontend/src/app/pages/inside-page/inside-page.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-  selector: "app-inside-page",
-  imports: [],
-  templateUrl: "./inside-page.component.html",
-  styleUrl: "./inside-page.component.scss",
-})
-export class InsidePageComponent {}
